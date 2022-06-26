222d0c0e0d5c02a5327e8b0e1e97862dec248a04
rubvitor <rubvitor@users.noreply.github.com>
2022-06-26 12:46:13 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 run_cmd_pull="docker pull rabbitmq:3-management"
-run_cmd_run="docker run -d --name rabbit-server -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=admin2017 -p 5672:5672 -p 15672:15672 rabbitmq:3-management"
+run_cmd_run="docker run -d --name rabbit-server -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=guest -p 5672:5672 -p 15672:15672 rabbitmq:3-management"
 
 >&2 echo "Pulling rabbitmq:3-management"
 exec $run_cmd_pull
